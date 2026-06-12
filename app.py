9ecc54e0422becfdb855973e299a3214287d635c
erikfosse <erikdavidfosse@gmail.com>
2026-06-12 21:33:44 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = QWidget()
+    window.show()
+
+    app.exec()
+
+if __name__ == "__main__":
+    main()
